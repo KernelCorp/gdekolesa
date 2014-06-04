60b5f51f9e91d44ad888ec251816632d9f59bbae
max <mk@kernel-corp.com>
2014-06-04 15:50:54 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gdekolesa::Application.config.secret_key_base = '881c6764f604b376b3a535b2d81c68d9633fa5ce5dde1f1bafeebcf170b86167082ea81bafaff31505e903661c27953fe6ad64a67661b740e8f896dadbe2d30f'
